2080aebd93e6583e96ff122c8aa01d9724766445
Hanna_Soika <Hanna_Soika@epam.com>
2019-11-22 21:44:39 +0300

    fix for path to *.log

--- test_frame_hs/configurator.py
+++ test_frame_hs/configurator.py
@@ -1,15 +1,10 @@
-import os
 
 
 class Config:
     def __init__(self, env):
-        print(os.getcwd())
         with open('config.json') as f:
-            print(os.getcwd())
             self.config = eval(f.read())
-            print(os.getcwd())
         self.config = self.config[env]
-        print(os.getcwd())
 
     def get_db_url(self):
         return self.config['database']
